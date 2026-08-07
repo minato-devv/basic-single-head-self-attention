6b4c593fdb66d8efa95aebc269f5c611583c254d
minato-devv <q8xj9gs8hs@privaterelay.appleid.com>
2026-08-07 18:13:49 -0400

    improve variable naming consistency, apply set to input tokens for uniqueness

--- attention-v0.1.py
+++ attention-v0.1.py
@@ -4,21 +4,22 @@ import torch.nn
 
 input = "The quick brown dog jumped over the lazy fox"
 tokens = input.replace(',', '').split()
-vocab = {t: i for i, t in enumerate(sorted(tokens))}
+seq_len = len(tokens)
+vocab = {t: i for i, t in enumerate(sorted(set(tokens)))}
 tokenized_input = torch.tensor([vocab[t] for t in tokens])
 print("\n============ Tokenized input ============")
 print(tokenized_input)
 
-embedding_dim = 5
-embedding_weights = torch.nn.Embedding(len(vocab), embedding_dim)
+embed_dim = 5
+embedding_weights = torch.nn.Embedding(seq_len, embed_dim)
 embeddings = embedding_weights(tokenized_input)
 print("\n============ Embeddings (1, seq_len, embed_dim) ============")
 print(embeddings)
 
-weight_dim = 4
-W_q = torch.nn.Parameter(torch.randn(embedding_dim, weight_dim))
-W_k = torch.nn.Parameter(torch.randn(embedding_dim, weight_dim))
-W_v = torch.nn.Parameter(torch.randn(embedding_dim, weight_dim))
+head_dim = 4
+W_q = torch.nn.Parameter(torch.randn(embed_dim, head_dim))
+W_k = torch.nn.Parameter(torch.randn(embed_dim, head_dim))
+W_v = torch.nn.Parameter(torch.randn(embed_dim, head_dim))
 print("\n============ Q/K/V Weights (embed_dim, head_dim) ============")
 print(W_q)
 
@@ -29,7 +30,7 @@ print("\n============ Q/K/V Tensors (seq_len, head_dim) ============")
 print(Q)
 
 att_scores = Q @ K.transpose(-2,-1)
-att_weights = torch.nn.functional.softmax(att_scores / math.sqrt(embedding_dim), dim=-1)
+att_weights = torch.nn.functional.softmax(att_scores / math.sqrt(embed_dim), dim=-1)
 print("\n============ Attention weights (seq_len, seq_len) ============")
 print(att_weights)
 
